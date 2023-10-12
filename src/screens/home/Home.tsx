626af5d5b1672702205a894ecfee2183d600b6c9
Enguerrand ROBERT <enguerrand.robert@sii.fr>
2023-10-12 18:06:40 +0200

    Fix(Home): fixed text size

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
     const msg: string = 'This is a test project for the Three JS in a React App :)'
 
-    return <div style={{ color: 'black' }}>{msg}</div>
+    return <div style={{ color: 'black', fontSize: '1rem' }}>{msg}</div>
 }
